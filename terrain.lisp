0defc3fc6f29d966642ba7382df6479db0a00c68
cbaggers <chris.bagley@gmail.com>
2017-07-25 19:35:48 +0200

    start organising into passes

--- terrain.lisp
+++ terrain.lisp
@@ -97,128 +97,176 @@
 (defvar *virtual-pipe-area* 20f0)
 (defvar *virtual-pipe-length* 1f0)
 
-(defun-g calc-new-rain ((pos :vec2)
-                        (time-delta :float))
-  (+ (water-at pos)
-     (* time-delta
-        (rain pos)
-        *rain-rate-scale-factor*)))
+(defun-g add-rain ((current-water-height :float)
+                   (rain :float)
+                   (time-delta :float))
+  (+ current-water-height
+     (* time-delta rain *rain-rate-scale-factor*)))
 
 ;; ∆hL(x, y) = bt(x, y) + d1(x, y) − bt(x−1, y) − d1 (x−1, y)
 
 (defun-g height-diff ((pos-terrain-height :float)
-                      (pos-water-depth :float)
+                      (pos-water-height :float)
                       (offset-terrain-height :float)
-                      (offset-water-depth :float))
-  (- (+ pos-terrain-height pos-water-depth)
-     (+ offset-terrain-height offset-water-depth)))
+                      (offset-water-height :float))
+  (- (+ pos-terrain-height pos-water-height)
+     (+ offset-terrain-height offset-water-height)))
 
 (defvar *gravity* -9.81)
 
-(defun-g flux-to-offset ((pos :vec2)
-                         (flux-index :int)
-                         (offset :vec2)
+(defun-g flux-to-offset ((offset-data :vec4)
+                         (offset-flux :float)
                          (time-delta :float)
                          (pos-terrain-height :float)
-                         (pos-water-depth :float)
+                         (pos-water-height :float)
                          (height-water-sediment-map :sampler-2d)
                          (water-flux-map :sampler-2d))
-  (let* ((offset-pos (+ pos offset))
-         (offset-data (texture height-water-sediment-map offset-pos))
-         (offset-terrain-height (x offset-data))
-         (offset-water-depth (y offset-data)))
-
-    (max 0 (+ (aref (texture water-flux-map offset-pos) flux-index)
+  (let* ((offset-terrain-height (x offset-data))
+         (offset-water-height (y offset-data)))
+    (max 0 (+ offset-flux
               (* time-delta
                  *virtual-pipe-area*
                  (/ (* *gravity*
                        (height-diff pos-terrain-height
-                                    pos-water-depth
+                                    pos-water-height
                                     offset-terrain-height
-                                    offset-water-depth))
+                                    offset-water-height))
                     *virtual-pipe-length*))))))
 
-(defun-g calc-flux ((pos :vec2)
-                    (time-delta :float)
-                    (height-water-sediment-map :sampler-2d)
-                    (water-flux-map :sampler-2d))
-  (let* ((pos-data (texture height-water-sediment-map pos))
-         (pos-terrain-height (x pos-data))
-         (pos-water-depth (y pos-data))
-         ;;
-         (flux-left (flux-to-offset pos 0 (v! -1 0) time-delta
-                                    pos-terrain-height
-                                    pos-water-depth
-                                    height-water-sediment-map
-                                    water-flux-map))
-         (flux-right (flux-to-offset pos 1 (v! 1 0) time-delta
-                                    pos-terrain-height
-                                    pos-water-depth
-                                    height-water-sediment-map
-                                    water-flux-map))
-         (flux-top (flux-to-offset pos 2 (v! 0 1) time-delta
-                                    pos-terrain-height
-                                    pos-water-depth
-                                    height-water-sediment-map
-                                    water-flux-map))
-         (flux-bottom (flux-to-offset pos 3 (v! 0 -1) time-delta
-                                      pos-terrain-height
-                                      pos-water-depth
-                                      height-water-sediment-map
-                                      water-flux-map))
-         (flux (v! flux-left flux-right flux-top flux-bottom)))
-    (v! (* (k-factor pos time-delta flux pos-water-depth))
-        (* (k-factor pos time-delta flux pos-water-depth))
-        (* (k-factor pos time-delta flux pos-water-depth))
-        (* (k-factor pos time-delta flux pos-water-depth)))))
-
-
-(defun-g calc-water-delta ((water-flux-map :sampler-2d)
-                           (pos :vec2))
-  (let ((flux-left (texture water-flux-map (+ pos (v! -1 0))))
-        (flux-right (texture water-flux-map (+ pos (v! 1 0))))
-        (flux-top (texture water-flux-map (+ pos (v! 0 1))))
-        (flux-bottom (texture water-flux-map (+ pos (v! 0 -1))))
-        (flux-pos (texture water-flux-map pos)))
-    (- (+ (x flux-right)
-          (y flux-left)
-          (z flux-bottom)
-          (w flux-top))
-       (+ (x flux-pos)
-          (y flux-pos)
-          (z flux-pos)
-          (w flux-pos)))))
-
-(defun-g calc-new-water-depth ((height-water-sediment-map :sampler-2d)
-                               (water-flux-map :sampler-2d)
-                               (pos :vec2))
-  (let ((current-depth (y (texture height-water-sediment-map pos)))
-        (water-delta (calc-water-delta water-flux-map pos)))
-    (+ current-depth
-       (/ water-delta
-          (* *virtual-pipe-length* *virtual-pipe-length*)))))
-
-(defun-g k-factor ((pos :vec2)
-                   (time-delta :float)
+(defun-g k-factor ((time-delta :float)
                    (flux :vec4)
-                   (water-depth :float))
-  (max 1 (/ (* water-depth
+                   (water-height :float))
+  (max 1 (/ (* water-height
                *virtual-pipe-length*
                *virtual-pipe-length*)
             (* time-delta
                (+ (x flux) (y flux) (z flux) (w flux))))))
+
+(defun-g erosion-step-0 ((uv :vec2)
+                          &uniform (time-delta :float) (tex-size :float)
+                          (height-water-sediment-map :sampler-2d)
+                          (flux-map :sampler-2d))
+  (let* ((tex-step (/ 1.0 tex-size))
+         (uv-l (+ uv (v! (- tex-size) 0)))
+         (uv-r (+ uv (v! tex-size 0)))
+         (uv-t (+ uv (v! 0 tex-size)))
+         (uv-b (+ uv (v! 0 (- tex-size))))
+         ;;
+         ;; data
+         (data (texture height-water-sediment-map uv))
+         (data-l (texture height-water-sediment-map uv-l))
+         (data-r (texture height-water-sediment-map uv-r))
+         (data-t (texture height-water-sediment-map uv-t))
+         (data-b (texture height-water-sediment-map uv-b))
+         (flux (texture flux-map uv))
+         (flux-l (x flux))
+         (flux-r (y flux))
+         (flux-t (z flux))
+         (flux-b (w flux))
+         ;;
+         ;; unpack
+         (terrain-height (x data))
+         (water-height (y data))
+         (sediment-amount (z data))
+         ;;
+         ;; Rain
+         (rain (rain uv))
+         (water-plus-rain (add-rain water-height rain time-delta))
+         ;;
+         ;; flux
+         (new-flux-l (flux-to-offset data-l flux-l time-delta
+                                     terrain-height
+                                     water-height
+                                     height-water-sediment-map
+                                     flux-map))
+         (new-flux-r (flux-to-offset data-r flux-r time-delta
+                                     terrain-height
+                                     water-height
+                                     height-water-sediment-map
+                                     flux-map))
+         (new-flux-t (flux-to-offset data-t flux-t time-delta
+                                     terrain-height
+                                     water-height
+                                     height-water-sediment-map
+                                     flux-map))
+         (new-flux-b (flux-to-offset data-b flux-b time-delta
+                                     terrain-height
+                                     water-height
+                                     height-water-sediment-map
+                                     flux-map))
+         (new-flux (v! new-flux-l new-flux-r new-flux-t new-flux-b))
+         (new-flux (v! (* (k-factor time-delta new-flux water-height))
+                       (* (k-factor time-delta new-flux water-height))
+                       (* (k-factor time-delta new-flux water-height))
+                       (* (k-factor time-delta new-flux water-height)))))
+    (values (v! terrain-height water-plus-rain sediment-amount)
+            new-flux)))
+
+
+(defun-g calc-water-delta ((flux :vec4)
+                           (flux-at-l :vec4)
+                           (flux-at-r :vec4)
+                           (flux-at-t :vec4)
+                           (flux-at-b :vec4))
+  (- (+ (y flux-at-l) (x flux-at-r) (w flux-at-t) (z flux-at-b))
+     (+ (x flux) (y flux) (z flux) (w flux))))
+
+(defun-g calc-new-water-height ((water-depth :float)
+                                (water-delta :float))
+  (+ water-depth
+     (/ water-delta
+        (* *virtual-pipe-length* *virtual-pipe-length*))))
 
 ;;                            x    y    z    w
 ;; • water outflow flux f = ( fL , fR , fT , fB ),
 
 ;; ∆Wx = ½(fR(x−1, y) − fL(x, y) + fR(x, y) − fL(x + 1, y))
 
-(defun-g calc-something ()
-  (let ((flux-left (texture water-flux-map (+ pos (v! -1 0))))
-        (flux-right (texture water-flux-map (+ pos (v! 1 0))))
-        (flux-pos (texture water-flux-map pos))))
+(defun-g calc-velocity-2d ((flux :vec4)
+                           (flux-at-l :vec4)
+                           (flux-at-r :vec4)
+                           (flux-at-t :vec4)
+                           (flux-at-b :vec4))
   (* 0.5
-     (v! (+ (- (y flux-left) (x flux-pos))
-            (- (y flux-pos) (x flux-right)))
-         (+ (- (y flux-top) (x flux-pos))
-            (- (y flux-pos) (x flux-bottom))))))
+     (v! (+ (- (y flux-at-l) (x flux))
+            (- (y flux) (x flux-at-r)))
+         (+ (- (y flux-at-t) (x flux))
+            (- (y flux) (x flux-at-b))))))
+
+(defun-g erosion-step-1 ((uv :vec2)
+                         &uniform (time-delta :float) (tex-size :float)
+                         (height-water-sediment-map :sampler-2d)
+                         (flux-map :sampler-2d))
+  (let* ((tex-step (/ 1.0 tex-size))
+         (uv-l (+ uv (v! (- tex-size) 0)))
+         (uv-r (+ uv (v! tex-size 0)))
+         (uv-t (+ uv (v! 0 tex-size)))
+         (uv-b (+ uv (v! 0 (- tex-size))))
+         ;;
+         ;; data
+         (data (texture height-water-sediment-map uv))
+         (data-l (texture height-water-sediment-map uv-l))
+         (data-r (texture height-water-sediment-map uv-r))
+         (data-t (texture height-water-sediment-map uv-t))
+         (data-b (texture height-water-sediment-map uv-b))
+         (flux (texture flux-map uv))
+         (flux-at-l (texture flux-map uv-l))
+         (flux-at-r (texture flux-map uv-r))
+         (flux-at-t (texture flux-map uv-t))
+         (flux-at-b (texture flux-map uv-b))
+         ;;
+         ;; unpack
+         (terrain-height (x data))
+         (water-height (y data))
+         (sediment-amount (z data))
+         ;;
+         ;;
+         (water-delta (calc-water-delta
+                       flux flux-at-l flux-at-r flux-at-t flux-at-b))
+         (new-water-height (calc-new-water-height water-height water-delta ))
+         ;;
+         ;; Velocity
+         (velocity-2d (calc-velocity-2d
+                       flux flux-at-l flux-at-r flux-at-t flux-at-b))
+         )
+    0))
