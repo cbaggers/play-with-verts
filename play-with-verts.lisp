71525f9d6ac765670f2bec8b9b23760ed85d1c3c
cbaggers <chris.bagley@gmail.com>
2017-11-09 00:14:48 +0100

    after stream

--- play-with-verts.lisp
+++ play-with-verts.lisp
@@ -4,31 +4,20 @@
 
 (defvar *some-sampler* nil)
 
-
 (defun reset ()
   (setf *some-sampler* (tex "wat0.png")))
 
-
-(defun-g simple-v ((vert :vec2))
-  (values (v! vert 0 1)
-          (+ (vec2 0.5) (* vert 0.5))))
-
-
-(defun-g simple-f ((uv :vec2) &uniform (sam :sampler-2d))
+(defun-g goodluck-f ((uv :vec2) &uniform (sam :sampler-2d))
   (texture sam uv))
 
-
-(defpipeline-g simple ()
-  (simple-v :vec2)
-  (simple-f :vec2))
-
+(defpipeline-g nada (:points)
+  :fragment (goodluck-f :vec2))
 
 (defun game-step ()
   (setf (viewport-resolution (current-viewport))
         (surface-resolution (current-surface (cepl-context))))
   (as-frame
-    (map-g #'simple (get-quad-stream-v2) :sam *some-sampler*)))
-
+    (map-g #'nada *bs* :sam *some-sampler*)))
 
 (def-simple-main-loop play (:on-start #'reset)
   (game-step))
