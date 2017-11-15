99b3c88f96e1805f9e966996ee72289c8f47cd13
cbaggers <chris.bagley@gmail.com>
2017-11-15 19:49:46 +0100

    better naming

--- play-with-verts.lisp
+++ play-with-verts.lisp
@@ -9,17 +9,17 @@
   (setf *some-sampler* (tex "wat0.png"))
   (setf *bs* (make-buffer-stream nil :primitive :points)))
 
-(defun-g goodluck-f ((uv :vec2) &uniform (sam :sampler-2d))
-  (texture sam uv))
+(defun-g blit ((uv :vec2) &uniform (sam :sampler-2d))
+  (texture sam (* uv (v! 1 -1))))
 
-(defpipeline-g nada (:points)
-  :fragment (goodluck-f :vec2))
+(defpipeline-g blitter (:points)
+  :fragment (blit :vec2))
 
 (defun game-step ()
   (setf (viewport-resolution (current-viewport))
         (surface-resolution (current-surface (cepl-context))))
   (as-frame
-    (map-g #'nada *bs* :sam *some-sampler*)))
+    (map-g #'blitter *bs* :sam *some-sampler*)))
 
 (def-simple-main-loop play (:on-start #'reset)
   (game-step))
